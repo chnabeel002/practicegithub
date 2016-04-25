e0ad99cda4deae8e0342bacd22ede036d3ae3cf6
chnabeel002 <chnabeel003@gmail.com>
2016-04-25 17:40:51 +0500

    Your first commit.....................................

---
+++ Github/src/abc.java
@@ -0,0 +1,13 @@
+
+public class abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HIIIIIIIIIII");
+		System.out.println("How are you??");
+		System.out.println("Are you there???");
+		
+
+	}
+
+}
